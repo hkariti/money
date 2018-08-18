53dda7ab15aace65fffd9daf0204cf477fbd9d6f
Hagai Kariti <hkariti@gmail.com>
2018-08-18 18:48:10 +0300

    Add CORS settings to make angular fetch work

--- money/settings.py
+++ money/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,7 +72,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'money.wsgi.application'
 
-
+# Cross Origin Resource Sharing
+CORS_ORIGIN_WHITELIST = [
+        'localhost:4200', # Allow angular dev
+]
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
