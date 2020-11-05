c4371ea991215689e77c43f37d5d89bba7884f33
Hagai Kariti <hkariti@gmail.com>
2020-11-05 20:22:14 +0200

    re-allow null category

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -14,7 +14,7 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'title']
 
 class TransactionSerializer(serializers.ModelSerializer):
-    category = serializers.SlugRelatedField(slug_field='title', queryset=Category.objects.all())
+    category = serializers.SlugRelatedField(slug_field='title', queryset=Category.objects.all(), allow_null=True)
     from_account = serializers.SlugRelatedField(slug_field='name', queryset=Account.objects.all(), allow_null=True)
     to_account = serializers.SlugRelatedField(slug_field='name', queryset=Account.objects.all(), allow_null=True)
 
