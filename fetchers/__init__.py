474a0268c8c413813f56df455e1c3f292f10e4c0
Hagai Kariti <hkariti@gmail.com>
2021-08-02 00:05:41 +0300

    Added recurring expense fetcher

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -3,11 +3,13 @@ from .utils import FetchException
 from . import leumi
 from . import leumicard
 from . import cal
+from . import recurring
 
 BACKENDS = {
         'leumi': leumi,
         'leumicard': leumicard,
         'cal': cal,
+        'recurring': recurring,
         }
 
 def get_backend(name):
