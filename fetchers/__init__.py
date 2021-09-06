34aa461e9555e062ccab095c0a131332ec96eadf
Hagai Kariti <hkariti@gmail.com>
2021-09-07 00:43:52 +0300

    Add otsar fetcher that uses Selenium

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -4,12 +4,14 @@ from . import leumi
 from . import leumicard
 from . import cal
 from . import recurring
+from . import otsar
 
 BACKENDS = {
         'leumi': leumi,
         'leumicard': leumicard,
         'cal': cal,
         'recurring': recurring,
+        'otsar': otsar,
         }
 
 def get_backend(name):
